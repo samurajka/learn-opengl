f0adf67e5f35f8de167532e44509d54cec37f40d
samurajka <tischlingerferda@gmail.com>
2026-08-27 10:41:28 +0200

    merge glfw into repo, not as submodule

---
+++ glfw/build/tests/CMakeFiles/empty.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jedi75/learning/learn-opengl/glfw/deps/tinycthread.c" "tests/CMakeFiles/empty.dir/__/deps/tinycthread.c.o" "gcc" "tests/CMakeFiles/empty.dir/__/deps/tinycthread.c.o.d"
+  "/home/jedi75/learning/learn-opengl/glfw/tests/empty.c" "tests/CMakeFiles/empty.dir/empty.c.o" "gcc" "tests/CMakeFiles/empty.dir/empty.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jedi75/learning/learn-opengl/glfw/build/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
